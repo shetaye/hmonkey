7a895b3480e867033da85e7ad13aa98a3dcd817e
Elias Vera-Jimenez <eliasrvjimenez@gmail.com>
2022-03-21 14:27:50 -0500

    Made simple webpage.

--- web/src/index.js
+++ web/src/index.js
@@ -2,20 +2,21 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class hMonkey extends Component {
- render (){
+export default class HMonkey extends Component {
+ render ()
+ {
   return( 
  <div> 
  <h1> Greetings </h1>
  <p> this is just filler, for now </p >
    <p> current user: {user} </p>
- </div>
-);
-}
-}
+ </div>)
+ }
+};
 
 const user = "Elias";
 
 ReactDOM.render(
- <hMonkey />, 
- document.getElementById('root'));
+ <HMonkey />, 
+ document.getElementById('root')
+);
