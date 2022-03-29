fc67d1930f6a87d82c0777b7bd71f38b0d4237ca
Elias Vera-Jimenez <eliasrvjimenez@gmail.com>
2022-03-28 21:33:29 -0500

    disabled darkmode, will be worked on later.

---
+++ web/src/darkmode.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import './style.css';
+
+/* will allow the user to change the background theme of the website,
+ * once I figure out how to make it work :/ hiding the button for now, until I can make it sit properly */
+
+
